b2b78cacd99daf45956bc7dcd382b481d1d49b50
Ngoc Quang Do <info.dongocquang@gmail.com>
2023-04-19 02:21:07 +0700

    up

--- BE/index.js
+++ BE/index.js
@@ -1,4 +1,7 @@
+const { query } = require('./db.js');
+
 const express = require('express');
+const fs = require('fs');
 const cors = require("cors");
 const app = express();
 let corsOptions = {
@@ -7,78 +10,159 @@ let corsOptions = {
 app.use(cors(corsOptions));
 const port = 3000;
 
-const dssv = require('./DSSV1.json');
+// const dssv = require('./DSSV.json');
+
+const bodyParser = require('body-parser');
+const { start } = require('repl');
+var urlParser = bodyParser.urlencoded({ extended: false });
+
 
 app.get('/', (req, res) => {
   res.send('Welecome to EXPRESS backend!!');
 });
 
-async function getStudents(page = 1, size = 10)
-{
-  let start = (page-1)*size;
-  sql = `select * from students order by MaSV limit ${start},${size}`;
+
+// functions
+async function getStudentsMysql(page = 1, size = 10) {
+  let start = (page - 1) * size;
+  let sql = `SELECT * FROM students ORDER BY MaSV LIMIT ${start},${size}`;
   let data = [];
-  await db.query(sql).then((rows)=>{
-    data= rows;
+  await query(sql).then((rows) => {
+    data = rows;
   });
-  return data;
+  sql1 = `SELECT COUNT(*) AS 'TotalRecord' FROM students`;
+  let totRecord = 0;
+  await query(sql1).then((rows) => {
+    totRecord = rows[0].TotalRecord;
+  });
+  return {
+    data: data,
+    TotalRecord: totRecord
+  };
 }
 
-async function createStudent(student)
-{
-  let sql = `insert into students(MaSV, TenSV, NgaySinh, DiaChi) values('${student.MaSV}','${student.TenSV}','${student.NgaySinh}','${student.DiaChi}')`;
-  let result;
-  await db.query(sql).then((res)=>{
-    result = res;
-  });
-  return result;
-}
-
-async function updateStudent(student)
-{
-  let sql = `update students set TenSV = '${student.TenSV}', NgaySinh = '${student.NgaySinh}', DiaChi = '${student.DiaChi}' where MaSV = '${student.MaSV}'`;
-  let result;
-  await db.query(sql).then((res)=>{
-    result = res;
-  });
-  return result;
-}
 
 //GET 
 app.get('/students', (req, res) => {
-  // res.send(Object.values(dssv));
+  res.send(Object.values(dssv));
 });
 
+// GET Json
+// app.get('/students/:mssv', (req, res) => {
+//   console.log(req.params.mssv);
+//   let i = 0;
+//   for (i = 0; i < dssv.length; i++) {
+//     if (dssv[i].codeStudent == req.params.mssv) {
+//       break;
+//     }
+//   }
+//   if (i < dssv.length) {
+//     res.send(dssv[i]);
+//   }
+//   else {
+//     res.send("Not Fond!!");
+//   }
+// });
+
+
+
+
+// GET mySql
 app.get('/students_mysql', async (req, res) => {
   page = req.query.page;
   size = req.query.size;
-  if(page !=undefined && size != undefined)
-  {
-    const lst = await getStudents(page, size);
-    res.send(lst);
-  }
-  else {
+  if (page != undefined && size != undefined) {
+    const ret = await getStudentsMysql(page, size);
+    res.send(ret);
+  } else {
     res.send("Not Fond!!");
   }
 });
 
-// POST
-app.post('/students', async (req, res) => {
-  let student = req.body;
-  const result = await createStudent(student);
-  res.send(result);
-});
+// //POST Add new student
+// app.post("/students", urlParser, (req, res) => {
+//   var sv = req.body;
+//   var result = dssv.find(item => item.MaSV === sv.MaSV);
+//   console.log(result);
+//   if (result != null || result != undefined) {
+//     var obj = {
+//       success: false, msg: "Mã SV bị trùng!"
+//     };
+//     res.send(obj);
+//   }
+//   else {
+//     dssv.push(sv);
+//     fs.writeFile('DSSV.json', JSON.stringify(dssv), err => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       else {
+//         console.log("OK");
+//       }
+//     });
 
-// PUT
-app.put('/students', async (req, res) => {
-  let student = req.body;
-  const result = await updateStudent(student);
-  res.send(result);
-});
+//     var obj = {
+//       success: true, msg: "Add new student success!"
+//     };
+//     res.send(obj);
+//   }
 
-app.delete('/students/:mssv', (req, res) => {
+// });
 
 
-});
+// app.put('/students/:mssv', urlParser, (req, res) => {
+//   const mssv = req.params.mssv; // get the mssv from the URL parameter
+//   const updatedStudent = req.body; // get the updated student data from the request body
+
+//   // Find the index of the student with the specified mssv value
+//   const index = dssv.findIndex(student => student.MaSV === mssv);
+//   console.log(index);
+//   if (index === -1) {
+//     // If the student is not found, send a 404 response
+//     res.status(404).send('Student not found');
+//   } else {
+//     // Update the properties of the student object at the specified index
+//     dssv[index].HoTen = updatedStudent.HoTen;
+//     dssv[index].Lop = updatedStudent.Lop;
+//     dssv[index].GioiTinh = updatedStudent.GioiTinh;
+//     dssv[index].NgaySinh = updatedStudent.NgaySinh;
+
+//     // Write the updated data to the JSON file
+//     fs.writeFile('DSSV.json', JSON.stringify(dssv), err => {
+//       if (err) {
+//         console.log(err);
+//         res.status(500).send('Server error');
+//       } else {
+//         res.send('Student updated successfully');
+//       }
+//     });
+//   }
+// });
+
+
+
+// app.delete("/students", urlParser, (req, res) => {
+//   var sv = req.body;
+//   var i = dssv.findIndex(item => item.MaSV === sv.MaSV);
+//   console.log(i);
+//   if (i != null || i != undefined) {
+//     dssv.splice(i, 1);
+//     fs.writeFile('DSSV.json', JSON.stringify(dssv), err => {
+//       if (err) {
+//         console.log(err);
+//       }
+//       else {
+//         console.log("OK");
+//       }
+//       var obj = {
+//         success: true, msg: "Xoá thành công!"
+//       };
+//       res.send(obj);
+//     });
+//   }
+
+
+// });
+
 
 app.listen(port, () => console.log(`App is running at port ${port}`));
--- FE/dssv.js
+++ FE/dssv.js
@@ -2,42 +2,137 @@ lst = [];
 curItem = null;
 
 $(function () {
-  getStudents();
+  getStudentsMysql();
 });
 
+let canChangeTotal = 40;
 
+function goToPage(page) {
+  const maxPage = Math.ceil(numStudents / pageSize);
+  if (page < 1) {
+    page = 1;
+  } else if (page > maxPage) {
+    page = maxPage;
+  }
+  cPage = page;
+  getStudentsMysql();
+}
 
-function getStudents() {
+function goNext() {
+  cPage++;
+  getStudentsMysql();
+}
 
+function goPrev() {
+  cPage--;
+  getStudentsMysql();
+}
+
+function getStudentsMysql() {
   $.ajax({
-    type: "GET",
+    method: "GET",
     url: "http://localhost:3000/students_mysql",
     data: {
-      page: 1,
-      size: 5
+      page: cPage,
+      size: 10
     }
-  }).done(function (data) {
-    lst = [];
-    data.forEach((dssv, i) => {
-      dssv.STT = i + 1;
-      lst.push(dssv);
-    });
-
-    if (lst.length > 0) {
-      $("#tbodySV").html("");
-      $("#svTemplate").tmpl(lst).appendTo("#tbodySV");
-    }
-    else {
-      let str = "<caption>No data fond!</caption>";
-      $("#tbodySV").html(str);
-    }
-  }).fail(function () {
-    str = "<caption>Error data!</caption>";
-    $("#tbodySV").html(str);
-  });
-
+  })
+    .done(function (res) {
+      let data = res.data;
+      lst = [];
+      data.forEach((dssv, i) => {
+        dssv.STT = (cPage - 1) * size + i + 1;
+        lst.push(dssv);
+      });
+      if (lst.length > 0) {
+        $("#tbodySV").html("");
+        $("#svTemplate").tmpl(lst).appendTo("#tbodySV");
+      }
+      else {
+        str = "<caption>No DATA FOUND</caption>"
+        $("#tbodySV").html(str);
+      }
+      curPage = cPage;
+      totPage = parseInt(res.TotalRecord / 10) + 1;
+      canChangeTotal = res.TotalRecord;
+      // console.log(res.TotalRecord);
+      // console.log(canChangeTotal);
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) { console.log(textStatus) });
+  // .error(function (err) {
+  //   str = "<caption>Error...</caption>"
+  //   $("#tbodySV").html(str);
+  // });
 
 }
+console.log(canChangeTotal);
+
+// Get the pagination element and the numStudents variable
+const pagination = document.querySelector('.pagination');
+let numStudents = canChangeTotal;
+
+// Define the page size (e.g. 10 students per page)
+const pageSize = 10;
+
+// Calculate the initial number of pages
+const numPages = parseInt(numStudents / pageSize);
+// console.log(numPages);
+
+// Update the pagination HTML with the correct number of page links
+let html = '';
+html += `<li class="page-item">
+            <a class="page-link" href="#" onclick="goPrev()" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>`;
+for (let i = 1; i <= numPages; i++) {
+  html += `<li class="page-item"><a class="page-link" href="#" onclick="goToPage(${i})">${i}</a></li>`;
+}
+html += `<li class="page-item">
+            <a class="page-link" href="#" onclick="goNext()" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>`;
+pagination.innerHTML = html;
+
+const size = 10; // set the number of records to display per page
+let cPage = 1; // initialize the current page to 1
+
+// Update the goToPage() function to take the new number of pages into account
+
+
+// getStudentsJson
+// function getStudentsJson() {
+//   fetch("http://localhost:3000/students")
+//     .then(res => { return res.json(); })
+//     .then(data => {
+//       lst = [];
+//       let i = 1;
+//       data.forEach(sv => {
+//         sv.STT = i++;
+//         lst.push(sv);
+//       });
+//       if (lst.length > 0) {
+//         $("#tbodySV").html("");
+//         $("#svTemplate").tmpl(lst).appendTo("#tbodySV");
+//       }
+//       else {
+//         str = "<caption>No DATA FOUND</caption>"
+//         $("#tbodySV").html(str);
+//       }
+//     })
+//     .catch(err => {
+//       str = "<caption>ERROR .....</caption>"
+//       $("#tbodySV").html(str);
+//     })
+// }
+
+
+// getStudentsMysql
+
+
+
+
 
 // function openModal(mssv, index) {
 //   fetch('http://localhost:3000/students')
@@ -50,20 +145,17 @@ function getStudents() {
 //         if (i === index) {
 //           console.log(student);
 //           document.querySelector("#currentID").value = index;
-//           document.querySelector("#txtCode").value = student.codeStudent;
-//           document.querySelector("#txtName").value = student.name;
-//           document.querySelector("#txtClass").value = student.class;
-//           document.querySelector("#date").value = student.birthday;
+//           document.querySelector("#txtCodeEdit").value = student.MaSV;
+//           document.querySelector("#txtNameEdit").value = student.HoTen;
+//           document.querySelector("#txtClassEdit").value = student.Lop;
+//           document.querySelector("#dateEdit").value = student.NgaySinh;
 
-//           let genderRadios = document.getElementsByName("gender");
-//           genderRadios.forEach(radio => {
-//             if (radio.value === student.gender && student.gender == "Nam") {
-//               radio.checked = true;
-//             }
-//             if (radio.value === student.gender && student.gender == "Nữ") {
-//               radio.checked = true;
-//             }
-//           });
+//           if (student.GioiTinh === "Nam") {
+//             document.querySelector('#male').checked = true;
+//           }
+//           if (student.GioiTinh === "Nữ") {
+//             document.querySelector('#female').checked = true;
+//           }
 
 //           // Show the modal
 //           $('#modalEdit').modal('show');
@@ -95,26 +187,73 @@ function getStudents() {
 //       modal.style.display = "none";
 //     }
 //   }
+// }
 
+// function addStudent() {
+
+//   gt = $('input[name="gender"]:checked').val();
+//   $.ajax({
+//     method: "POST",
+//     url: "http://localhost:3000/students",
+//     data: {
+//       "MaSV": $("#txtCodeAdd").val(),
+//       "HoTen": $("#txtNameAdd").val(),
+//       "Lop": $("#txtClassAdd").val(),
+//       "GioiTinh": gt,
+//       "NgaySinh": $("#dateAdd").val(),
+//     }
+//   })
+//     .done(function (res) {
+//       if (res.success) alert(res.msg);
+//       else alert(res.msg);
+//     }).fail(function (jqXHR, textStatus, errorThrown) { console.log(textStatus) });
 
 // }
 
 
+// function updateStudent() {
+//   var gt = $('input[name="genderEdit"]:checked').val();
+//   var data = {
+//     "MaSV": $("#txtCodeEdit").val(),
+//     "HoTen": $("#txtNameEdit").val(),
+//     "Lop": $("#txtClassEdit").val(),
+//     "GioiTinh": gt,
+//     "NgaySinh": $("#dateEdit").val(),
+//   };
+//   var studentCode = $("#txtCodeEdit").val();
+//   console.log(studentCode);
+//   $.ajax({
+//     method: "PUT",
+//     url: "http://localhost:3000/students/" + studentCode,
+//     data: data
+//   })
+//     .done(function (res) {
+//       if (res.success) alert(res.msg);
+//       else alert("Update student success");
+//     }).fail(function (jqXHR, textStatus, errorThrown) { console.log(textStatus) });
 
-// function deleteStudent(mssv, index) {
-
-//   // $.ajax({
-//   //   url: `http://localhost:3000/students/${mssv}`,
-//   //   type: 'DELETE',
-//   //   success: function(result) {
-//   //     console.log(result);
-//   //     // Handle the successful deletion of the student here
-//   //   },
-//   //   error: function(xhr, status, error) {
-//   //     console.error(error);
-//   //     // Handle any errors that occurred during the deletion here
-//   //   }
-//   // });
 // }
+
+// function deleteStudent(mssv) {
+//   if (confirm("Bạn có chắc chắn muốn xóa không?")) {
+//     $.ajax({
+//       method: "DELETE",
+//       url: "http://localhost:3000/students",
+//       data: {
+//         "MaSV": mssv,
+//       }
+//     })
+//       .done(function (res) {
+//         if (res.success) {
+//           alert(res.msg);
+//           getStudents();
+//         }
+//         else alert(res.msg);
+//       }).fail(function (jqXHR, textStatus, errorThrown) { console.log(textStatus) });
+//   }
+// }
+
+
+
 
 
